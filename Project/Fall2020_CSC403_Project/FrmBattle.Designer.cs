f60745db18243b6bd65e95c3377e7cdf0a6059eb
SaiDileep <sra063@email.latech.edu>
2023-10-28 17:15:21 -0500

    Added advetisment URL and Timer clunt dwn.

--- Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
@@ -31,11 +31,11 @@
             this.lblEnemyHealthFull = new System.Windows.Forms.Label();
             this.tmrFinalBattle = new System.Windows.Forms.Timer(this.components);
             this.advertisingPanel = new System.Windows.Forms.Panel();
-            this.RedirectLink = new System.Windows.Forms.LinkLabel();
             this.AdClose = new System.Windows.Forms.TextBox();
             this.picBossBattle = new System.Windows.Forms.PictureBox();
             this.picEnemy = new System.Windows.Forms.PictureBox();
             this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.advertisingPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBossBattle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picEnemy)).BeginInit();
@@ -101,28 +101,19 @@
             this.advertisingPanel.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.advertisingPanel.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.AD1;
             this.advertisingPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.advertisingPanel.Controls.Add(this.RedirectLink);
             this.advertisingPanel.Controls.Add(this.AdClose);
+            this.advertisingPanel.Cursor = System.Windows.Forms.Cursors.Hand;
             this.advertisingPanel.Enabled = false;
             this.advertisingPanel.Location = new System.Drawing.Point(52, 52);
             this.advertisingPanel.Name = "advertisingPanel";
             this.advertisingPanel.Size = new System.Drawing.Size(720, 458);
             this.advertisingPanel.TabIndex = 8;
             this.advertisingPanel.Visible = false;
-            // 
-            // RedirectLink
-            // 
-            this.RedirectLink.AutoSize = true;
-            this.RedirectLink.Location = new System.Drawing.Point(301, 200);
-            this.RedirectLink.Name = "RedirectLink";
-            this.RedirectLink.Size = new System.Drawing.Size(55, 13);
-            this.RedirectLink.TabIndex = 1;
-            this.RedirectLink.TabStop = true;
-            this.RedirectLink.Text = "linkLabel1";
-            this.RedirectLink.Visible = false;
+            this.advertisingPanel.Click += new System.EventHandler(this.advertisingPanel_Click);
             // 
             // AdClose
             // 
+            this.AdClose.Cursor = System.Windows.Forms.Cursors.Hand;
             this.AdClose.Dock = System.Windows.Forms.DockStyle.Right;
             this.AdClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AdClose.ForeColor = System.Drawing.Color.Red;
@@ -169,6 +160,11 @@
             this.picPlayer.TabIndex = 0;
             this.picPlayer.TabStop = false;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // FrmBattle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -211,6 +207,6 @@
     private System.Windows.Forms.Timer tmrFinalBattle;
         private System.Windows.Forms.Panel advertisingPanel;
         private System.Windows.Forms.TextBox AdClose;
-        private System.Windows.Forms.LinkLabel RedirectLink;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Project/Fall2020_CSC403_Project/FrmBattle.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.cs
@@ -1,6 +1,7 @@
 ﻿using Fall2020_CSC403_Project.code;
 using Fall2020_CSC403_Project.Properties;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Media;
 using System.Windows.Forms;
@@ -10,8 +11,11 @@ namespace Fall2020_CSC403_Project {
     public static FrmBattle instance = null;
     private Enemy enemy;
     private Player player;
-
-    private FrmBattle() {
+    private List<string> texts = new List<string> { "5", "4", "3", "2", "X" };
+    private int currentIndex = 0;
+    private bool ad3start = false;
+    string urlToOpen = "";
+        private FrmBattle() {
       InitializeComponent();
       player = Game.player;
     }
@@ -50,11 +54,7 @@ namespace Fall2020_CSC403_Project {
       simpleSound.Play();
 
       tmrFinalBattle.Enabled = true;
-      string val = BackColor.Name.ToString();
-      if (val == "Red")
-      {
-          AdVisible(val);
-      }
+      
     }
 
     public static FrmBattle GetInstance(Enemy enemy) {
@@ -102,26 +102,46 @@ namespace Fall2020_CSC403_Project {
     private void tmrFinalBattle_Tick(object sender, EventArgs e) {
       picBossBattle.Visible = false;
       tmrFinalBattle.Enabled = false;
+      string val = BackColor.Name.ToString();
+          if (val == "Red")
+            {
+                AdVisible(val);
+            }
     }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex < texts.Count)
+            {
+                AdClose.Text = texts[currentIndex];
+                currentIndex++;
+            }
+            else
+            {
+                // When you reach the end of the list, reset the index to start over.
+                currentIndex = 0;
+            }
+            if (AdClose.Text == "X")
+            {
+                timer1.Stop();
+            }
+        }
 
-    private void advertisingPanel_Paint(object sender, PaintEventArgs e)
-    {
-
-    }
-
-       
         private void AdVisible(string color)
         {
+            timer1.Start();
             if (color == "Green")
             {
+                 urlToOpen= "https://latechsports.com/sports/football"; // Replace with your desired URL
                 advertisingPanel.BackgroundImage = Resources.AD1;
             }
             else if (color == "Orange")
             {
+                urlToOpen = "https://www.chick-fil-a.com/"; // Replace with your desired URL
                 advertisingPanel.BackgroundImage = Resources.AD2;
             }
             else if (color == "Red")
             {
+                urlToOpen = "https://www.adidas.com/us"; // Replace with your desired URL
                 advertisingPanel.BackgroundImage = Resources.AD3;
             }
            
@@ -131,9 +151,30 @@ namespace Fall2020_CSC403_Project {
 
         private void AdClose_Click(object sender, EventArgs e)
         {
-            advertisingPanel.Visible = false;
-            advertisingPanel.Enabled = false;
+            if (AdClose.Text == "X")
+            {
+                advertisingPanel.Visible = false;
+                advertisingPanel.Enabled = false;
+                ad3start = false;
+                timer1.Stop();
+            }
+        }
 
+        private void advertisingPanel_Click(object sender, EventArgs e)
+        {
+            // Specify the URL you want to open in the default web browser
+           
+
+            try
+            {
+                // Open the default web browser with the specified URL
+                System.Diagnostics.Process.Start(urlToOpen);
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur when opening the web browser
+                MessageBox.Show("An error occurred while opening the web browser: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
